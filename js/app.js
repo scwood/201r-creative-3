487e71af82a131b9d3b4a31c83ca4a4d3f46e600
Spencer Wood <spencercwood@gmail.com>
2017-03-01 21:27:51 -0700

    Add working app

--- js/app.js
+++ js/app.js
@@ -1,7 +1,61 @@
-// TODO add ng-animate
 angular.module('app', [])
-  .controller('Controller', ['$scope', Controller]);
+  .controller('todoController', ['$scope', todoController]);
 
-function Controller($scope) {
-  $scope.greeting = 'hello world'
+function todoController($scope) {
+  $scope.todos = [];
+  $scope.newTodo = generateBlankTodo();
+  loadTodosFromStorage();
+
+  $scope.addTodo = function () {
+    if ($scope.newTodo.text.trim() === '') {
+      $scope.error = 'Todo must have some text';
+      return;
+    }
+    var labels = $scope.newTodo.labels.split(',').map(function (label) {
+      return label.trim();
+    });
+    var uniqueLabels = labels.filter(function (label, index) {
+      return labels.indexOf(label) === index && label.trim() !== '';
+    });
+    $scope.newTodo.labels = uniqueLabels;
+    $scope.todos.unshift($scope.newTodo);
+    $scope.newTodo = generateBlankTodo();
+    saveTodosToStorage();
+    $scope.error = null;
+  }
+
+  $scope.deleteTodo = function (todo) {
+    $scope.todos.splice($scope.todos.indexOf(todo), 1)
+    saveTodosToStorage();
+  }
+
+  $scope.toggleDone = function (todo) {
+    todo.isDone = !todo.isDone;
+    saveTodosToStorage();
+  }
+
+  $scope.calculateOutstanding = function () {
+    var outstanding = $scope.todos.filter(function (todo) {
+      return !todo.isDone;
+    });
+    $scope.outstanding = outstanding.length;
+    return outstanding;
+  }
+
+  function loadTodosFromStorage() {
+    var todosFromStorage = localStorage.getItem('todos');
+    if (todosFromStorage) {
+      JSON.parse(todosFromStorage).forEach(function (todo) {
+        $scope.todos.push(todo);
+      });
+    }
+  }
+
+  function saveTodosToStorage() {
+    localStorage.setItem('todos', JSON.stringify($scope.todos));
+  }
+
+  function generateBlankTodo() {
+    return { text: '', isDone: false, labels: '' };
+  }
 }
